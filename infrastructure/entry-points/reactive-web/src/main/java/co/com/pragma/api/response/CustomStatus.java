cdd510c4bf67d2dbee370ad8ffd33e1be4668b9f
0103juan <0103juan@gmail.com>
2025-09-02 09:05:40 -0500

    Updates custom status codes for API responses
    Updates the `CustomStatus` enum to include more descriptive codes and messages for API responses.
    
    This improves error handling and provides clearer feedback to the client.

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/response/CustomStatus.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/response/CustomStatus.java
@@ -8,15 +8,15 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CustomStatus {
 
-    USER_CREATED_SUCCESSFULLY(HttpStatus.CREATED, "USER_CREATED_SUCCESSFULLY", "Usuario creado exitosamente."),
-    USER_FOUND_SUCCESSFULLY(HttpStatus.OK, "USER_FOUND_SUCCESSFULLY", "Usuario encontrado exitosamente."),
+    USER_CREATED_SUCCESSFULLY(HttpStatus.CREATED, "USER_CREATED_201", "Usuario creado exitosamente."),
+    USER_FOUND_SUCCESSFULLY(HttpStatus.OK, "USER_FOUND_200", "Usuario encontrado exitosamente."),
 
-    USER_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "USER_VALIDATION_ERROR", "La solicitud tiene errores de validación."),
-    INVALID_ROLE(HttpStatus.BAD_REQUEST, "INVALID_ROLE", "El rol especificado no es válido."),
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "El usuario solicitado no existe."),
-    USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER_ALREADY_EXISTS", "El correo electrónico o el número de documento ya están registrados."),
+    USER_VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "USER_VALIDATION_ERROR_400", "La solicitud tiene errores de validación."),
+    INVALID_ROLE(HttpStatus.BAD_REQUEST, "INVALID_ROLE_400", "El rol especificado no es válido."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND_400", "El usuario solicitado no existe."),
+    USER_ALREADY_EXISTS(HttpStatus.CONFLICT, "USER_ALREADY_EXISTS_409", "El correo electrónico o el número de documento ya están registrados."),
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", "Ocurrió un error inesperado en el servidor.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR_500", "Ocurrió un error inesperado en el servidor.");
 
     private final HttpStatus httpStatus;
     private final String code;
